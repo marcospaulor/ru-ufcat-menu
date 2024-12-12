37b6d7a884d639d4ed6ab234b07133616b9f2463
marcospaulor <dev.silva.marcos@gmail.com>
2024-12-12 00:46:42 -0300

    Replace initial page content with Login and UpdateMenu components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import Image from 'next/image'
+import Login from './pages/Login/Login'
+import UpdateMenu from './pages/UpdateMenu/UpdateMenu'
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-900 flex justify-center items-center">
-        <h1 className="text-white text-9xl">Initial page</h1>
-      </div>
+      <Login />
+      <UpdateMenu />
     </>
-  );
+  )
 }
