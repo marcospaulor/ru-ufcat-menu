20b9be12235be4817c5435fd7449153616f6a04a
marcospaulor <dev.silva.marcos@gmail.com>
2024-12-28 00:08:28 -0300

    Add LoadingScreen component and integrate it into AuthProvider and Home component for improved loading state handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-
 import { useAuth } from '@/app/hooks/useAuth'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import UpdateMenu from './(pages)/updateMenu/page'
 import Layout from './components/Layout'
+import LoadingScreen from './components/Loading/Loading'
 
 export default function Home() {
   const { user, loading } = useAuth()
@@ -18,7 +18,7 @@ export default function Home() {
   }, [user, loading, router])
 
   if (loading) {
-    return <p>Carregando...</p>
+    return <LoadingScreen />
   }
 
   if (!user) {
