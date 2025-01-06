4fc7fe9fb499350002097612d9acb6db28cf163a
marcospaulor <dev.silva.marcos@gmail.com>
2025-01-06 10:32:22 -0300

    Enhance useMenuLogic hook to manage meal categories dynamically and update Firestore with selected meals

--- src/app/hooks/useMenuLogic.ts
+++ src/app/hooks/useMenuLogic.ts
@@ -1,47 +1,58 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Food } from '@/app/interface/types'
+import useFoods from './useFoods'
 import { MealCategory } from '@/app/interface/types'
+import { firestoreService } from '@/app/services/FirestoreServices'
 
 type MealTime = 'Almoço' | 'Jantar'
 
 export function useMenuLogic() {
-  const [activeTab, setActiveTab] = useState('segunda')
+  const { foods } = useFoods() // Assumindo que useFoods retorna um objeto com 'foods'
+  const [activeTab, setActiveTab] = useState('segunda-feira')
   const [mealTime, setMealTime] = useState<MealTime | null>(null)
   const [selectedMeals, setSelectedMeals] = useState<
     Record<string, Record<MealTime, string[]>>
   >({
-    segunda: { Almoço: [], Jantar: [] },
-    terca: { Almoço: [], Jantar: [] },
-    quarta: { Almoço: [], Jantar: [] },
-    quinta: { Almoço: [], Jantar: [] },
-    sexta: { Almoço: [], Jantar: [] },
+    'segunda-feira': { Almoço: [], Jantar: [] },
+    'terca-feira': { Almoço: [], Jantar: [] },
+    'quarta-feira': { Almoço: [], Jantar: [] },
+    'quinta-feira': { Almoço: [], Jantar: [] },
+    'sexta-feira': { Almoço: [], Jantar: [] },
   })
 
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
   const [isAlertModalOpen, setIsAlertModalOpen] = useState(false)
   const [isSubmitModalOpen, setIsSubmitModalOpen] = useState(false)
 
+  const [categories, setCategories] = useState<MealCategory[]>([])
+
   const tabs = [
-    { id: 'segunda', title: 'Segunda-feira' },
-    { id: 'terca', title: 'Terça-feira' },
-    { id: 'quarta', title: 'Quarta-feira' },
-    { id: 'quinta', title: 'Quinta-feira' },
-    { id: 'sexta', title: 'Sexta-feira' },
+    { id: 'segunda-feira', title: 'Segunda-feira' },
+    { id: 'terca-feira', title: 'Terça-feira' },
+    { id: 'quarta-feira', title: 'Quarta-feira' },
+    { id: 'quinta-feira', title: 'Quinta-feira' },
+    { id: 'sexta-feira', title: 'Sexta-feira' },
   ]
 
-  const categories: MealCategory[] = [
-    {
-      category: 'Prato principal',
-      foods: ['Arroz', 'Feijão', 'Frango Assado'],
-    },
-    {
-      category: 'Vegano/Vegetariano',
-      foods: ['Salada de Grão-de-Bico', 'Tofu Grelhado'],
-    },
-    { category: 'Acompanhamento', foods: ['Batata Frita', 'Farofa'] },
-    { category: 'Guarnição', foods: ['Vinagrete', 'Molho à Campanha'] },
-    { category: 'Salada', foods: ['Alface', 'Tomate', 'Rúcula'] },
-    { category: 'Sobremesa', foods: ['Mousse de Maracujá', 'Pudim'] },
-  ]
+  useEffect(() => {
+    if (foods.length > 0) {
+      const uniqueCategories = new Set<string>()
+      const allFoods = new Map<string, string[]>()
+
+      foods.forEach((food: Food) => {
+        uniqueCategories.add(food.category)
+        if (!allFoods.has(food.category)) allFoods.set(food.category, [])
+        allFoods.get(food.category)?.push(food.name)
+      })
+
+      setCategories(
+        Array.from(uniqueCategories).map((category) => ({
+          category,
+          foods: allFoods.get(category) || [],
+        }))
+      )
+    }
+  }, [foods]) // Este efeito será recalculado sempre que 'foods' mudar
 
   const toggleFood = (food: string) => {
     if (!mealTime) {
@@ -66,21 +77,52 @@ export function useMenuLogic() {
 
   const clearSelection = () => {
     setSelectedMeals({
-      segunda: { Almoço: [], Jantar: [] },
-      terca: { Almoço: [], Jantar: [] },
-      quarta: { Almoço: [], Jantar: [] },
-      quinta: { Almoço: [], Jantar: [] },
-      sexta: { Almoço: [], Jantar: [] },
+      'segunda-feira': { Almoço: [], Jantar: [] },
+      'terca-feira': { Almoço: [], Jantar: [] },
+      'quarta-feira': { Almoço: [], Jantar: [] },
+      'quinta-feira': { Almoço: [], Jantar: [] },
+      'sexta-feira': { Almoço: [], Jantar: [] },
     })
   }
 
-  const handleFinalSubmit = () => {
-    console.log(
-      'Dados do cardápio enviados:',
-      JSON.stringify(selectedMeals, null, 2)
-    )
-    alert('Cardápio enviado com sucesso!')
-    setIsSubmitModalOpen(false)
+  const handleFinalSubmit = async () => {
+    try {
+      const menuData = Object.entries(selectedMeals).reduce(
+        (acc, [day, meals]) => {
+          acc[day] = {
+            almoco: {},
+            janta: {},
+          }
+
+          Object.entries(meals).forEach(([time, foods]) => {
+            const mealTime = time === 'Almoço' ? 'almoco' : 'janta'
+            foods.forEach((food) => {
+              const category = categories
+                .find((cat) => cat.foods.includes(food))
+                ?.category.toLowerCase()
+                .replace(' ', '_')
+              if (category) {
+                acc[day][mealTime][category] = food
+              }
+            })
+          })
+
+          return acc
+        },
+        {} as Record<
+          string,
+          { almoco: Record<string, string>; janta: Record<string, string> }
+        >
+      )
+
+      // Atualizar o menu no Firebase
+      await firestoreService.updateDocument('ru', 'menu', menuData)
+      alert('Cardápio enviado com sucesso!')
+      setIsSubmitModalOpen(false)
+    } catch (error) {
+      console.error('Erro ao enviar cardápio:', error)
+      alert('Ocorreu um erro ao enviar o cardápio.')
+    }
   }
 
   return {
