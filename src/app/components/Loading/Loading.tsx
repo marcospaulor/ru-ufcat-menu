df83ba7f87e991a0fc969560432bbf323f89a555
marcospaulor <dev.silva.marcos@gmail.com>
2025-01-07 15:14:45 -0300

    Change loading spinner color to orange

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ export default function LoadingScreen() {
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <div className="flex flex-col items-center">
         {/* Spinner */}
-        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <div className="w-16 h-16 border-4 border-orange-ufcat border-t-transparent rounded-full animate-spin"></div>
 
         {/* Texto de Loading */}
         <p className="mt-4 text-gray-600 font-semibold">Carregando...</p>
